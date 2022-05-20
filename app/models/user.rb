05132b9eaf06c1a422cc2cc8b75689fd68215e65
sskryu322 <sence0322@icloud.com>
2022-05-20 15:52:16 +0900

    バリデーションの追加設定完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_pseudonym_reading, presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_pseudonym_reading, presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
 end
