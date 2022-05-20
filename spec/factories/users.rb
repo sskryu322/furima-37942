d50254787db8390458ccf66d0911864e0c81ba87
sskryu322 <sence0322@icloud.com>
2022-05-20 21:43:40 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {'1a'+ Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name {"太郎"}
-    first_name {"山田"}
-    last_name_pseudonym_reading {"タロウ"}
-    first_name_pseudonym_reading {"ヤマダ"}
-    birthday {"1999-01-01"}
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name { '太郎' }
+    first_name { '山田' }
+    last_name_pseudonym_reading { 'タロウ' }
+    first_name_pseudonym_reading { 'ヤマダ' }
+    birthday { '1999-01-01' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,12 +40,14 @@ RSpec.describe User, type: :model do
       it 'last_name_pseudonym_readingが空では登録できない' do
         @user.last_name_pseudonym_reading = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name pseudonym reading can't be blank", "Last name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include("Last name pseudonym reading can't be blank",
+                                                      'Last name pseudonym reading is invalid')
       end
       it 'first_name_pseudonym_readingが空では登録できない' do
         @user.first_name_pseudonym_reading = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name pseudonym reading can't be blank", "First name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include("First name pseudonym reading can't be blank",
+                                                      'First name pseudonym reading is invalid')
       end
       it 'birthdayが空では登録できない' do
         @user.birthday = ''
@@ -53,76 +55,76 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
       it 'emailは@を含まないと登録できない' do
-        @user.email = "hoge.jp"
+        @user.email = 'hoge.jp'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'passwordが5文字以下では登録できない' do
         @user.password = 'a1234'
-        @user.password_confirmation = "a1234"
+        @user.password_confirmation = 'a1234'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが129文字以上では登録できない' do
         @user.password = '1a' + Faker::Internet.password(min_length: 129)
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
-        @user.password = "a123456"
-        @user.password_confirmation = "a12345678"
+        @user.password = 'a123456'
+        @user.password_confirmation = 'a12345678'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordが数字だけでは登録できない' do
         @user.password = '123456'
-        @user.password_confirmation = "123456"
+        @user.password_confirmation = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it 'passwordが英字だけでは登録できない' do
         @user.password = 'abcdef'
-        @user.password_confirmation = "abcdef"
+        @user.password_confirmation = 'abcdef'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it 'last_nameが英字では登録できない' do
         @user.last_name = 'yamada'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
       it 'first_nameが英字では登録できない' do
         @user.first_name = 'taro'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
       it 'last_name_pseudonym_readingがひらがなでは登録できない' do
         @user.last_name_pseudonym_reading = 'やまだ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include('Last name pseudonym reading is invalid')
       end
       it 'last_name_pseudonym_readingが英字では登録できない' do
         @user.last_name_pseudonym_reading = 'yamada'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include('Last name pseudonym reading is invalid')
       end
       it 'first_name_pseudonym_readingがひらがなでは登録できない' do
         @user.first_name_pseudonym_reading = 'たろう'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include('First name pseudonym reading is invalid')
       end
       it 'first_name_pseudonym_readingが英字では登録できない' do
         @user.first_name_pseudonym_reading = 'taro'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name pseudonym reading is invalid")
+        expect(@user.errors.full_messages).to include('First name pseudonym reading is invalid')
       end
     end
   end
