276928602cc01f74cfd898e5903a8b93d4daa69a
sskryu322 <sence0322@icloud.com>
2022-05-22 01:19:57 +0900

    ActiveHashの導入と設定完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :shipping_charge
+  belongs_to :shipment_source
+  belongs_to :date_shipment
   belongs_to :user
   has_one_attached :image
 
   validates :name, presence: true
   validates :content, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  validates :quality_id, presence: true
-  validates :shipping_charge_id, presence: true
-  validates :shipment_source_id, presence: true
-  validates :date_shipment_id, presence: true
+  validates :quality_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipment_source_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :date_shipment_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :image, presence: true
 end
