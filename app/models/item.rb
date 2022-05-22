1b98a7c69e4691709d0e400f7e19448d7ed8b770
sskryu322 <sence0322@icloud.com>
2022-05-22 20:54:48 +0900

    JSによる販売手数料と販売利益の実装完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :content, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true, numericality: { other_than: 1 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  validates :quality_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipment_source_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :date_shipment_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :quality_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :shipment_source_id, presence: true, numericality: { other_than: 1 }
+  validates :date_shipment_id, presence: true, numericality: { other_than: 1 }
   validates :image, presence: true
 end
