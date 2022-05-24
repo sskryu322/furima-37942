f619256079076f0d4829678529a336a744ff9b1a
sskryu322 <sence0322@icloud.com>
2022-05-24 15:15:56 +0900

    コメント指摘内容の修正(2)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
+    if current_user.id == item.user_id
       item.destroy
       redirect_to root_path
     end
@@ -53,6 +53,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
