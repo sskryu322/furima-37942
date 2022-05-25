a31500cc08d9151c22f85dfc960fea9623645e89
sskryu322 <sence0322@icloud.com>
2022-05-25 14:40:08 +0900

    purchase_recordsテーブルとshipping_addressesテーブルの作成とアソシエーションの記述完了

---
+++ db/migrate/20220525052653_create_shipping_addresses.rb
@@ -0,0 +1,14 @@
+class CreateShippingAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shipping_addresses do |t|
+      t.string :postal_code, null: false
+      t.integer :shipment_source_id , null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
+      t.string :building_name, null: false
+      t.string :telephone_number, null: false
+      t.references :purchase_record, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
