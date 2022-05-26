1e491148938065e728d1df9c13b0bdb9db89743f
sskryu322 <sence0322@icloud.com>
2022-05-26 15:38:30 +0900

    ログイン状況によるエラーハンドリング完了

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,12 +1,14 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create, :move_to_index]
+  before_action :move_to_index, only: [:index, :create]
   
+
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
   
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
@@ -20,5 +22,15 @@ class PurchaseRecordsController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:postal_code, :shipment_source_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if @item.purchase_record.present? || current_user.id == @item.user.id
+      redirect_to root_path 
+    end
   end
 end
